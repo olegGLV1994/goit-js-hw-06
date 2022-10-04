26d31164872399291cdc7d00456aafb65a8a458b
Oleg Gavelya <oleggavelya@gmail.com>
2022-10-04 17:31:17 +0300

    2 exercise

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,27 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const gredientsEl = document.querySelector("#ingredients");
+
+// for (let i = 0; i < ingredients.length; i+=1) {
+//   const itemEl = document.createElement("li");
+//   itemEl.textContent = ingredients[i];
+//   itemEl.classList.add("item");
+//  console.log(itemEl);
+// }
+
+const gredients = ingredients.map((gredient) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = gredient;
+  itemEl.classList.add("item");
+  return itemEl;
+});
+console.log(gredients);
+
+gredientsEl.append(...gredients);
