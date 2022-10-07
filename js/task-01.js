406bcc6e0f3fb5a3f0d683728150307fdda92807
Oleg Gavelya <oleggavelya@gmail.com>
2022-10-07 20:52:32 +0300

    work 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,21 @@
-const categoriesEl = document.querySelectorAll("#categories ul");
-console.log(`Number of categories: ${categoriesEl.length}`);
+// Number of categories: 3
 
-const titleAnimalsEl = document.querySelectorAll("h2");
-console.log(`Category: ${titleAnimalsEl[0].textContent}`);
+// Category: Animals
+// Elements: 4
 
-const animalsEl = document.querySelector(".item ul");
-const animalItemEl = animalsEl.children;
-console.log(`Element: ${animalItemEl.length}`);
-console.log(`Category: ${titleAnimalsEl[1].textContent}`);
-console.log(`Elements: ${categoriesEl[1].children.length}`);
-console.log(`Category: ${titleAnimalsEl[2].textContent}`);
-console.log(`Element: ${categoriesEl[2].children.length}`);
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+const itemEl = document.querySelectorAll(".item");
+console.log(
+  "Number of categories:",
+  document.querySelectorAll("#categories ul").length
+);
+
+itemEl.forEach((item) => {
+  console.log("Category:", item.querySelector("h2").textContent);
+  console.log("Elements:", item.querySelectorAll("li").length);
+});
